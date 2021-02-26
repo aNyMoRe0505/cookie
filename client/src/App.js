1d954013b6be0334754273fecf18a8fe3ae6a500
Paul Wang <anymore0505@gmail.com>
2021-02-26 17:55:51 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
+const API_HOST = 'http://localhost:3000'
+
 function App() {
+  const [result, setResult] = useState();
+
+  const setCookie = () => {
+    fetch(`${API_HOST}/set-cookie`, {
+      credentials: 'include',
+    });
+  };
+
+  const readCookie = () => {
+    fetch(`${API_HOST}/read-cookie`, {
+      credentials: 'include',
+    })
+      .then(res => res.json())
+      .then(jsonResult => {
+        setResult(jsonResult);
+      });
+  };
+
   return (
     <div>
-      hello
+      <button
+        type="button"
+        onClick={setCookie}>
+        fetch /set-cookie
+      </button>
+      <button
+        type="button"
+        onClick={readCookie}>
+        fetch /read-cookie
+      </button>
+      {result && <div>{JSON.stringify(result)}</div>}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,37 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.set({
+    'Access-Control-Allow-Origin': 'http://localhost:5000',
+    'Access-Control-Allow-Credentials': true,
+  });
+
+  next();
+});
+
+app.get('/read-cookie', (req, res) => {
+  res.status(200);
+  res.json({
+    message: req.cookies,
+  });
+});
+
+app.get('/set-cookie', (req, res) => {
+  res.cookie('testCookie', 'testCookie');
+
+  res.status(200);
+  res.json({
+    message: 'success',
+  });
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Example app listening at http://localhost:${port}`);
 });
